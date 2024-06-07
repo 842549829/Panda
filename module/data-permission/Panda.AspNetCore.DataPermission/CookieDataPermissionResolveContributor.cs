fec4b9ec07882b1fbacea38956bb4d6a4ac95e0d
liuxiaoji <842549829@qq.com>
2024-06-07 17:14:19 +0800

    style:delete file

---
+++ module/data-permission/Panda.AspNetCore.DataPermission/CookieDataPermissionResolveContributor.cs
@@ -0,0 +1,20 @@
+﻿using Panda.DataPermission.Abstractions.DataPermission;
+
+namespace Panda.AspNetCore.DataPermission;
+
+public class CookieDataPermissionResolveContributor : HttpDataPermissionResolveContributorBase
+{
+    public const string ContributorName = "Cookie";
+
+    /// <summary>
+    /// 参与者名称
+    /// </summary>
+    public override string Name => ContributorName;
+        
+    protected override Task<(string?, Panda.DataPermission.Abstractions.DataPermission.DataPermission?)> GetDataPermissionFromHttpContextOrNullAsync(IDataPermissionResolveContext context, HttpContext httpContext)
+    {
+        var code  = httpContext.Request.Cookies[context.GetPandaAspDataPermissionOptions().DataPermissionCodeKey];
+        var dataPermission = httpContext.Request.Cookies[context.GetPandaAspDataPermissionOptions().DataPermissionKey]?.ToDataPermission();
+        return Task.FromResult((code, dataPermission));
+    }
+}
---
+++ module/data-permission/Panda.AspNetCore.DataPermission/HttpDataPermissionResolveContributorBase.cs
@@ -0,0 +1,40 @@
+﻿using Panda.DataPermission;
+using Panda.DataPermission.Abstractions.DataPermission;
+using Volo.Abp;
+
+namespace Panda.AspNetCore.DataPermission;
+
+public abstract class HttpDataPermissionResolveContributorBase : DataPermissionResolveContributorBase
+{
+    public override async Task ResolveAsync(IDataPermissionResolveContext context)
+    {
+        var httpContext = context.GetHttpContext();
+        if (httpContext == null)
+        {
+            return;
+        }
+
+        try
+        {
+            await ResolveFromHttpContextAsync(context, httpContext);
+        }
+        catch (Exception e)
+        {
+            context.ServiceProvider
+                .GetRequiredService<ILogger<HttpDataPermissionResolveContributorBase>>()
+                .LogWarning(e.ToString());
+        }
+    }
+
+    protected virtual async Task ResolveFromHttpContextAsync(IDataPermissionResolveContext context, HttpContext httpContext)
+    {
+        var (code, dataPermission) = await GetDataPermissionFromHttpContextOrNullAsync(context, httpContext);
+        if (dataPermission != null)
+        {
+            context.DataPermissionCode = code;
+            context.DataPermission = dataPermission;
+        }
+    }
+
+    protected abstract Task<(string?, Panda.DataPermission.Abstractions.DataPermission.DataPermission?)> GetDataPermissionFromHttpContextOrNullAsync(IDataPermissionResolveContext context, HttpContext httpContext);
+}
